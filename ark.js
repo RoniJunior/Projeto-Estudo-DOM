418607ed667c9dbce75054b44cc8969772065077
Roni Carlos <Roni.Carlos@edlo.com.br>
2024-11-11 17:16:48 -0300

    Primeira parte projeto DOM

---
+++ ark.js
@@ -0,0 +1,6 @@
+var vel = 70.5;
+console.log('A velocidade do seu carro é 46(vel)km/h')
+if(vel > 60) { //condição simples!
+    console.log('Você ultrapassou o limite permitido, MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança, cuide-se!')
